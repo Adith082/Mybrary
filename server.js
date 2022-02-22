5c0a45ccbd99779aa8ab0e889c2d18e1a42c5159
Adith082 <adibhasan35@gmail.com>
2022-02-22 11:38:36 +0600

    initial author routes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
-const authorRouter = require("./routes/authors")
+const authorRouter = require("./routes/authors");
 
 if(process.env.NODE_ENV !=  "production"){
     require("dotenv").config();
@@ -14,7 +15,7 @@ app.set("views",__dirname + "/views");
 app.set("layout","layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({limit:"10mb",extended:false}));
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL , {useNewUrlParser:true});
 const db = mongoose.connection;
